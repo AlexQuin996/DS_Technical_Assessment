1f240bcac69a4ff0448861e33e3fd05347355dac
Alexander Quintana <77287792+AlexQuin996@users.noreply.github.com>
2025-12-07 13:54:55 -0700

    Initial commit

---
+++ DS_Technical_Assessment/Data_Processing_Transformation_Task/main.py
@@ -0,0 +1,15 @@
+from totalSpendPerCustomer import totalSpendPerCustomer
+from top5CustomersByTotalSpend import top5CustomersByTotalSpend
+from writeToJson import writeToJson
+
+# Calculates spending for all customers in the csv file
+totalSpendingPerCustomer = totalSpendPerCustomer()
+
+# Gets the top 5 customers by total spend
+top5Customers = top5CustomersByTotalSpend(totalSpendingPerCustomer)
+
+# Converts the dataframe to a list of dictionaries to be written to the JSON file
+finalResults = top5Customers.to_dict(orient='records')
+
+# Writes the list of dictionaries to a JSON file
+writeToJson(finalResults)
---
+++ DS_Technical_Assessment/Data_Processing_Transformation_Task/top5CustomersByTotalSpend.py
@@ -0,0 +1,2 @@
+def top5CustomersByTotalSpend(df):
+    return df.head(5) # Gets the top 5 customers by total spend, already sorted in totalSpendPerCustomer.py
