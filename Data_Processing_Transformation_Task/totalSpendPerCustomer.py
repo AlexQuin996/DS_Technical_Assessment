ed92d4271a9b2ff993244761d9e29bcca5485478
Alexander Quintana <77287792+AlexQuin996@users.noreply.github.com>
2025-12-14 10:23:49 -0500

    Clarified comment

--- Data_Processing_Transformation_Task/totalSpendPerCustomer.py
+++ Data_Processing_Transformation_Task/totalSpendPerCustomer.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def totalSpendPerCustomer(csv_file='../transactions.csv'): # Stores csv outside of the current directory
     df = pd.read_csv(csv_file) # Read the csv file and convert it into a pandas dataframe
-    # Calculates the total spend per customer in the csv file and sorts the values in descending order
+    # Calculates the total spend per customer in the csv file and sorts the results in descending order via amount
     calculateTotalSpendPerCustomer = df.groupby('customer_id')['amount'].sum().sort_values(ascending=False).reset_index() # Using reset_index() so customer_id is a column, not an index
     dfWithRenamedColumn = calculateTotalSpendPerCustomer.rename(columns={'amount': 'total_spend'}) # Rename the column 'amount' to 'total_spend'
     return dfWithRenamedColumn
